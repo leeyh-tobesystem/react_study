5050d26733eb56d0d3995654cf151536a15d21dc
leeyh <leeyh@tobesystem.co.kr>
2024-06-25 16:32:40 +0900

    3장 마무리

--- chapter3/src/Say.jsx
+++ chapter3/src/Say.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
 
 const Say = () => {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('empty');
   const onClickEnter = () => setMessage('Hi');
   const onClickLeave = () => setMessage('Bye');
+
+  const [color, setColor] = useState('black');
+
   return (
     <div>
       <button onClick={onClickEnter}>in</button>
       <button onClick={onClickLeave}>out</button>
-      <h1>{message}</h1>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        Red
+      </button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        Green
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        Blue
+      </button>
     </div>
   );
 };
