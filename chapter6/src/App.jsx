deaf6954bcc445022b650f71f38e6d9ba2cd31fc
leeyh <leeyh@tobesystem.co.kr>
2024-06-28 13:18:44 +0900

    6장

---
+++ chapter6/src/App.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+import Iteration from './Iteration';
+
+class App extends Component {
+  render() {
+    return <Iteration></Iteration>;
+  }
+}
+export default App;
